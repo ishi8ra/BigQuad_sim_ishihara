dc4cc718e3616381dee0e468915dcca79970f9d7
ishihara-plasticity <fishihara41@gmail.com>
2023-11-23 12:58:27 +0900

    save before fix in reference to matlab

--- controller/lqr_controller.py
+++ controller/lqr_controller.py
@@ -1,77 +1,81 @@
+import csv
 import numpy as np
 from tools.Mathfunction import Mathfunction as MF
 from scipy import linalg
-import csv
-import pprint
-
-# 初期設定
-r = 2 / 3  # r(λ)=2/3Lp 長:0.3m
-g = 9.8  # 重力加速度
-
-A = np.array(
-    [
-        [0, 1, 0, 0, 0, 0, 0, 0],
-        [0, 0, g, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 0, 0, -g, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-        [0, 0, 0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0, 0, 0],
-    ]
-)
-
-Bl = np.array(
-    [
-        [0, 0, 0],
-        [0, 0, 0],
-        [1, 0, 0],
-        [0, 0, 0],
-        [0, 0, 0],
-        [0, 1, 0],
-        [0, 0, 0],
-        [0, 0, 1],
-    ]
-)
-
-# 重みの決定   Q=[x, xd, β, y, yd, γ, z, zd]
-Q = np.diag([1, 1, 1, 1, 1, 1, 1, 1])
-R = np.diag([1, 1, 1])
-
-
-# lqr法
-def lqr(A, B, Q, R):
-    P = linalg.solve_continuous_are(A, B, Q, R)  # Ricatti方程式を解いてる?
-    K = linalg.inv(R).dot(B.T).dot(P)
-    E = linalg.eigvals(A - B.dot(K))
-
-    return P, K, E
-
-
-P, K, E = lqr(A, Bl, Q, R)  # このKはMatlabの結果通りになってる
-
-# P, K, E = control.lqr(A, Bl, Q, R)
-
-# K = np.array([[1, 1.4518, 5.4272, 0, 0, 0, 0, 0], # Kを手打ち
-#               [0, 0, 0, -1, -1.4518, 5.4272, 0, 0],
-#               [0, 0, 0, 0, 0, 0, 1, 1.7321]])
-
-# 目標値の設定（7行目は目標の高さz）
-q_ref = np.array([[0], [0], [0], [0], [0], [0], [1], [0]])
 
 
 class Lqr_Controller:
     def __init__(self, dt):
         self.dt = dt
-        self.gravity_calcel = 9.81
-        self.rad2deg = 180 / np.pi
+        self.r = 2 / 3  # r(λ)=2/3Lp 長:0.3m
+        self.g = 9.8  # 重力加速度
 
         # * initialize input values
         self.input_acc = 0.0
         self.input_Wb = np.zeros(3)
 
-    # def set_dt(self, dt):
-    #     self.dt = dt
+        # self.lqr_init() 一旦呼び出さないようにする
+
+        self.K = np.array([
+            [1.00000000000000, 1.45175395145263, 5.42718872423573, 8.97157015039534e-17,
+                2.36934578759692e-16, -1.51758575511169e-15, -1.83826241986066e-16, 3.25349858845081e-16],
+            [-4.54758256147348e-16, -2.68464342807294e-16, 1.87287620071113e-16, -0.999999999999999, -
+                1.36885555678166, 6.42217668469037, 1.48180184268838e-15, 3.30898740618047e-16],
+            [1.79859175550358e-16, 4.09738600794711e-16, -1.25760985716651e-16, 5.83691553045932e-17,
+                1.23204693030713e-16, -2.31044461374617e-16, 1.00000000000000, 1.73205080756888]
+        ])
+
+        self.q_ref = np.array([[0], [0], [0], [0], [0], [0], [1], [0]])
+
+    def set_dt(self, dt):
+        self.dt = dt
+
+    def lqr_init(self):
+        # 初期設定
+        self.r = 2 / 3  # r(λ)=2/3Lp 長:0.3m
+        self.g = 9.8  # 重力加速度
+
+        A = np.array(
+            [
+                [0, 1, 0, 0, 0, 0, 0, 0],
+                [0, 0, self.g, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 1, 0, 0, 0],
+                [0, 0, 0, 0, 0, -self.g, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+                [0, 0, 0, 0, 0, 0, 0, 1],
+                [0, 0, 0, 0, 0, 0, 0, 0],
+            ]
+        )
+
+        Bl = np.array(
+            [
+                [0, 0, 0],
+                [0, 0, 0],
+                [1, 0, 0],
+                [0, 0, 0],
+                [0, 0, 0],
+                [0, 1, 0],
+                [0, 0, 0],
+                [0, 0, 1],
+            ]
+        )
+
+        # 重みの決定   Q=[x, xd, β, y, yd, γ, z, zd]
+        Q = np.diag([1, 1, 1, 1, 1, 1, 1, 1])
+        R = np.diag([1, 1, 1])
+
+        # 目標値の設定（7行目は目標の高さz）
+        # self.q_ref = np.array([[0], [0], [0], [0], [0], [0], [1], [0]])
+
+        # self.P, self.K, self.E = self.lqr(A, Bl, Q, R)  # このKはMatlabの結果通りになってる
+
+    def lqr(self, A, B, Q, R):
+        P = linalg.solve_continuous_are(A, B, Q, R)  # Ricatti方程式を解いてる?
+        K = linalg.inv(R).dot(B.T).dot(P)
+        E = linalg.eigvals(A - B.dot(K))
+
+        return P, K, E
 
     def set_state(self, drone):
         self.P = drone.P.now
@@ -98,7 +102,7 @@ class Lqr_Controller:
         self.Euler_rateref = Euler_rate
 
     def controller(self):
-        q = np.array(
+        self.q_now = np.array(
             [
                 [self.P[0]],
                 [self.V[0]],
@@ -111,20 +115,20 @@ class Lqr_Controller:
             ]
         )
 
-        q_div = q - q_ref
-        u = -np.dot(K, q_div)
-        ad = u[2, 0]
-        a = ad + g + (g/2)*(self.Euler[1]*self.Euler[1] +
-                            self.Euler[2]*self.Euler[2])
+        q_div = self.q_now - self.q_ref
+        self.input = -np.dot(self.K, q_div)
+        ad = self.input[2, 0]
+        a = ad + self.g + (self.g/2)*(self.Euler[1]*self.Euler[1] +
+                                      self.Euler[2]*self.Euler[2])
 
         # nominal acceleraion
         self.input_acc = a
 
-        ox = u[1, 0]
-        oy = u[0, 0]
+        ox = self.input[1, 0]
+        oy = self.input[0, 0]
         oz = 0
 
         # calculate input Body angular velocity
-        self.input_Wb = np.array([[ox], [oy], [oz]], dtype=object)
+        self.input_Wb = np.array([ox, oy, oz], dtype=object)
 
         return self.input_acc, self.input_Wb
